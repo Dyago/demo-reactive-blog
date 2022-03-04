d5c7d3e98d2f684c6117ebc8894751b3a9658d83
Dyago Diaz <dyago.diaz@gmail.com>
2022-03-04 16:26:31 -0500

    initial code

---
+++ src/main/java/pe/com/bootcamp/demoreactiveblog/DemoReactiveBlogApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.demoreactiveblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactiveBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactiveBlogApplication.class, args);
+	}
+
+}
